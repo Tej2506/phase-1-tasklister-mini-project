8df7b3cfd1cd9d812d37edaf1b9893b0f05422c0
Basavanna <basavannatej25@gmail.com>
2023-12-07 23:46:06 +1030

    made changes for the lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit',(event)=>{
+    event.preventDefault()
+    let EneteredValue = event.target.new_task_description.value
+    handleValue(EneteredValue)
+
+  })
 });
+
+handleValue = (value)=>{
+  let ul = document.createElement('ul')
+  let item1 = document.createElement('li')
+  item1.textContent = `${value  }`
+  div = document.getElementById('list')
+  let btn = document.createElement('button')
+  btn.textContent = '  x'
+  ul.appendChild(item1)
+  item1.appendChild(btn)
+  div.appendChild(ul)
+  btn.addEventListener('click',(event)=>{
+  ul.remove()
+  })
+}
